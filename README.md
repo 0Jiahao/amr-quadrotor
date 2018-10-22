46e58fc9c77cf4c699e44843b9c4222f4f824c48
¨0jiahao¨ <gaaihoulam@outlook.com>
2018-10-22 17:24:54 +0200

    added front and bottom camera

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # amr-quadrotor
 A repository developed base on RotroS. The original repository is extended for AMR simulation.
+## How to install it
+- You need a **Ubuntu 16.04** and ROS** Kinetic** (http://wiki.ros.org/kinetic/Installation/Ubuntu)
+- Follow the following instruction to install **RotorS**(https://github.com/ethz-asl/rotors_simulator)
+```
+$ pip install future
+$ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu `lsb_release -sc` main" > /etc/apt/sources.list.d/ros-latest.list'
+$ wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
+$ sudo apt-get update
+$ sudo apt-get install ros-kinetic-desktop-full ros-kinetic-joy ros-kinetic-octomap-ros ros-kinetic-mavlink python-wstool python-catkin-tools protobuf-compiler libgoogle-glog-dev ros-kinetic-control-toolbox libgeographic-dev geographiclib-tools
+$ sudo rosdep init
+$ rosdep update
+$ source /opt/ros/kinetic/setup.bash
+$ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu `lsb_release -sc` main" > /etc/apt/sources.list.d/ros-latest.list'
+$ wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install python-catkin-tools
+$ mkdir [your_workspace]/src
+$ cd [your_workspace]src
+$ catkin_init_workspace  # initialize your catkin workspace
+$ wstool init
+$ wget https://raw.githubusercontent.com/ethz-asl/rotors_simulator/master/rotors_hil.rosinstall
+$ wstool merge rotors_hil.rosinstall
+$ wstool update
+```
+Since we do not need the rotors_simulator/rotors_hil_interface, and it always throw out error while compiling, please do
+```
+$ cd [your_workspace]/src/rotors_simulator/rotors_hil_interface
+$ touch CATKIN_IGNORE
+```
+After all above is done, clone this repository to the workspace
+```
+$ cd [your_workspace]/src
+$ git clone https://github.com/0Jiahao/amr_quadrotor.git
+```
+Finally, we can complie them all together
+```
+$ cd [your_workspace]
+$ catkin build
+```
+## Example
+Connect your xbox joystick with the computer, run
+```
+$ roslaunch ~/catkin_ws/src/amr_quadrotor/launch/amr_quadrotor.launch 
+```
+You can now fly the quadrotor with your joystick.
---
+++ amr_joy_interface/src/joy.cpp
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2015 Fadri Furrer, ASL, ETH Zurich, Switzerland
+ * Copyright 2015 Michael Burri, ASL, ETH Zurich, Switzerland
+ * Copyright 2015 Mina Kamel, ASL, ETH Zurich, Switzerland
+ * Copyright 2015 Janosch Nikolic, ASL, ETH Zurich, Switzerland
+ * Copyright 2015 Markus Achtelik, ASL, ETH Zurich, Switzerland
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#include "amr_joy_interface/joy.h"
+
+#include <mav_msgs/default_topics.h>
+
+Joy::Joy() {
+  ros::NodeHandle nh;
+  ros::NodeHandle pnh("~");
+  ctrl_pub_ = nh_.advertise<mav_msgs::RollPitchYawrateThrust> (
+    mav_msgs::default_topics::COMMAND_ROLL_PITCH_YAWRATE_THRUST, 10);
+
+  control_msg_.roll = 0;
+  control_msg_.pitch = 0;
+  control_msg_.yaw_rate = 0;
+  control_msg_.thrust.x = 0;
+  control_msg_.thrust.y = 0;
+  control_msg_.thrust.z = 0;
+
+  pnh.param("axis_roll_", axes_.roll, 2);
+  pnh.param("axis_pitch_", axes_.pitch, 3);
+  pnh.param("axis_yawrate_", axes_.yawrate, 0);
+  pnh.param("axis_thrust_", axes_.thrust, 1);
+
+  pnh.param("axis_direction_roll", axes_.roll_direction, -1);
+  pnh.param("axis_direction_pitch", axes_.pitch_direction, 1);
+  pnh.param("axis_direction_yawrate", axes_.yawrate_direction, 1);
+  pnh.param("axis_direction_thrust", axes_.thrust_direction, 1);
+
+  pnh.param("max_roll", max_.roll, 15.0 * M_PI / 180.0);  // [rad]
+  pnh.param("max_pitch", max_.pitch, 15.0 * M_PI / 180.0);  // [rad]
+  pnh.param("max_yawrate", max_.yawrate, 180.0 * M_PI / 180.0);  // [rad/s]
+  pnh.param("max_thrust", max_.thrust, 25.0);  // [N]
+
+  pnh.param("button_button0_", buttons_.button0, 3);
+  pnh.param("button_button1_", buttons_.button1, 4);
+  pnh.param("button_button2_", buttons_.button2, 5);
+  pnh.param("button_button3_", buttons_.button3, 10);
+  pnh.param("button_button4_", buttons_.button4, 7);
+  pnh.param("button_button5_", buttons_.button5, 8);
+
+  namespace_ = nh_.getNamespace();
+  joy_sub_ = nh_.subscribe("joy", 10, &Joy::JoyCallback, this);
+}
+
+
+void Joy::JoyCallback(const sensor_msgs::JoyConstPtr& msg) {
+  current_joy_ = *msg;
+  control_msg_.roll = msg->axes[axes_.roll] * max_.roll * axes_.roll_direction;
+  control_msg_.pitch = msg->axes[axes_.pitch] * max_.pitch * axes_.pitch_direction;
+
+  control_msg_.yaw_rate = msg->axes[axes_.yawrate] * max_.yawrate * axes_.yawrate_direction;
+
+  control_msg_.thrust.z = (msg->axes[axes_.thrust] + 1) * max_.thrust / 2.0 * axes_.thrust_direction;
+
+  ros::Time update_time = ros::Time::now();
+  control_msg_.header.stamp = update_time;
+  control_msg_.header.frame_id = "rotors_joy_frame";
+  Publish();
+}
+
+void Joy::Publish() {
+  ctrl_pub_.publish(control_msg_);
+}
+
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "rotors_joy_interface");
+  Joy joy;
+
+  ros::spin();
+
+  return 0;
+}
